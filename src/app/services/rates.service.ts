cb338c372b241b93969a829b46c7e4f2297c5adb
NancyStephanieVidal <gvidal.fany@gmail.com>
2026-02-26 15:05:04 -0600

    se suben enviroment

--- src/app/services/rates.service.ts
+++ src/app/services/rates.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environments/environment'; // ✅ RUTA COMPLETA
 
 export interface Destino {
   destino: string;
@@ -27,8 +28,7 @@ export interface CalculoResponse {
   providedIn: 'root'
 })
 export class RatesService {
-  // ✅ CAMBIA ESTO: usa la URL de Render
-  private apiUrl = 'https://odm-envios-calculadora.onrender.com/api';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
